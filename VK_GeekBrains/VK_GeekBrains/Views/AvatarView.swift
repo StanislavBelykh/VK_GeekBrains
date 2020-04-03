a1287a1dd87aa0ca4e5e0ccf9d4d3816142360ac
Stanislav_Belykh <apple.stas@outlook.com>
2020-04-03 12:40:20 +0300

    Shadow + Avatar

--- VK_GeekBrains/VK_GeekBrains/Views/AvatarView.swift
+++ VK_GeekBrains/VK_GeekBrains/Views/AvatarView.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2020 Станислав. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class  AvatarView: UIImageView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2
+        layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        layer.cornerRadius = frame.height/2
+        layer.masksToBounds = true
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        
+    }
+}
--- VK_GeekBrains/VK_GeekBrains/Views/ShadowView.swift
+++ VK_GeekBrains/VK_GeekBrains/Views/ShadowView.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2020 Станислав. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ShadowView: UIView {
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.height/2
+        layer.shadowColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        layer.shadowRadius = 8
+        layer.shadowOffset = .zero
+        layer.shadowOpacity = 1
+    }
+}
