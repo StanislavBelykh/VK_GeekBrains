beb38863e7721f56dca7adbf64fea1d112393c04
Stanislav_Belykh <apple.stas@outlook.com>
2020-04-03 15:09:43 +0300

    Добавил файл настроек для аватарок

---
+++ VK_GeekBrains/VK_GeekBrains/Controllers/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  VK_GeekBrains
+//
+//  Created by Станислав on 03.04.2020.
+//  Copyright © 2020 Станислав. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
